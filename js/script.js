e695d2338e7ddce4e61a219a5c43754ab82f9123
山本健太郎 <f19aa021@gmail.com>
2021-07-10 11:47:40 +0900

    add: top-cover image modified: header in about.html modified: other minor fixed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-const work = document.getElementsByClassName('work');
-const workCover = document.getElementsByClassName('work-cover');
-const workName = document.getElementsByClassName('work-name');
+// const workCover = document.getElementsByClassName('work-cover');
+// const workDescription = document.getElementsByClassName('work-description');
 
-for (let i = 0; i < work.length; i++) {
-    workCover[i].addEventListener('click', () => {
-        // work[i].classList.add('view');
-    });
-}
+// const toggle = () => {
+//     workDescription.classList.toggle('is-open');
+// }
+
+// for (let i = 0; i < workCover.length; i++) {
+//     workCover[i].addEventListener('click', toggle);
+// }
+
+var px_change = document.documentElement.clientHeight - 60;
+
+window.addEventListener('scroll', function(e) {
+    if ($(window).scrollTop() <= px_change) {
+        $("header").addClass("bg-clear");
+    } else if ($("header").hasClass("bg-clear")) {
+        $("header").removeClass("bg-clear");
+    }
+});
